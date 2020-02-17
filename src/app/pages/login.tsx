66b9e4954c122033582462b87c6850183b1b04ed
yanahiro <yanatch1982@gmail.com>
2020-02-17 20:34:15 +0900

    add validation

--- src/app/pages/login.tsx
+++ src/app/pages/login.tsx
@@ -2,37 +2,91 @@
 import * as React from 'react'
 import Router from 'next/router'
 import Layout from '../components/layout'
+import SimpleReactValidator from 'simple-react-validator';
 
-interface LoginProps {}
+interface LoginProps {
+}
 interface LoginState {
   credentials: {
     email: string
     password: string
   }
+  errors: {
+    email: boolean
+    password: boolean
+  }
   isLoading: boolean
 }
 
 class Login extends React.Component<LoginProps, LoginState> {
+
+  // ???????
+  validator: any
+
   constructor(props: LoginProps) {
     super(props)
     this.state = {
       credentials: {
-        email: null,
-        password: null,
+        email: "",
+        password: "",
       },
-      isLoading: false
+      errors: {
+        email: false,
+        password: false,
+      },
+      isLoading: false,
     }
+    this.validator = new SimpleReactValidator();
   }
 
   handleCredentialsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let { credentials } = this.state
     credentials[e.target.name] = e.target.value
-
     this.setState({ credentials })
+  }
+
+  handleCredentialsBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let { credentials, errors } = this.state
+    credentials[e.target.name] = e.target.value
+    if (!this.validator.fieldValid(e.target.name)) {
+      // errors update
+      errors[e.target.name] = true
+      // validation error
+      this.validator.showMessageFor(e.target.name)
+      this.forceUpdate();
+    } else {
+      errors[e.target.name] = false
+    }
+    // update errors status
+    this.setState({ errors })
+  }
+
+  handleCredentialFocus = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let { errors } = this.state
+    errors[e.target.name] = false
+    this.setState({ errors })
   }
 
   handleLoginSubmit = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault()
+    if (this.validator.allValid()) {
+      alert('Input validation success.')
+    } else {
+      // alert('Input validation error.')
+      // show message
+      this.validator.showMessages()
+      this.forceUpdate()
+      const validErrors = this.validator.getErrorMessages()
+      let { errors } = this.state
+      Object.keys(validErrors).forEach(validError => {
+        // console.log(validError)
+        errors[validError] = true
+      })
+      // update errors status
+      this.setState({ errors })
+
+      return false;
+    }
     this.setState({ isLoading: true })
 
     setTimeout(() => {
@@ -43,7 +97,6 @@ class Login extends React.Component<LoginProps, LoginState> {
   }
 
   public render() {
-
     return (
       <Layout title="Login" isHeader={false} isFooter={false}>
         <form action="">
@@ -55,20 +108,22 @@ class Login extends React.Component<LoginProps, LoginState> {
               <div className="field">
                 <label className="label">Email</label>
                 <p className="control has-icons-left">
-                  <input className="input" type="email" placeholder="Email" />
+                  <input className={`input ${this.state.errors.email ? 'is-danger' : ''}`} type="email" name="email" value={this.state.credentials.email} onChange={this.handleCredentialsChange} onBlur={this.handleCredentialsBlur} onFocus={this.handleCredentialFocus} placeholder="Email" />
                   <span className="icon is-small is-left">
                     <i className="fas fa-envelope"></i>
                   </span>
                 </p>
+                <div className="help is-danger">{this.validator.message('email', this.state.credentials.email, 'required|email')}</div>
               </div>
               <div className="field">
                 <label className="label">Password</label>
                 <p className="control has-icons-left">
-                  <input className="input" type="password" placeholder="Password" autoComplete="off" />
+                  <input className={`input ${this.state.errors.password ? 'is-danger' : ''}`}  type="password" name="password" value={this.state.credentials.password} onChange={this.handleCredentialsChange} onBlur={this.handleCredentialsBlur} onFocus={this.handleCredentialFocus} placeholder="Password" autoComplete="off" />
                   <span className="icon is-small is-left">
-                    <i className="fa fa-lock"></i>
+                    <i className="fas fa-lock"></i>
                   </span>
                 </p>
+                <div className="help is-danger">{this.validator.message('password', this.state.credentials.password, 'required')}</div>
               </div>
               <div className="field">
                 <p className="">
@@ -95,6 +150,10 @@ class Login extends React.Component<LoginProps, LoginState> {
 
           .field {
             padding-bottom: 1.5rem;
+          }
+
+          .icon svg {
+            height: 16px;
           }
         `}
         </style>
